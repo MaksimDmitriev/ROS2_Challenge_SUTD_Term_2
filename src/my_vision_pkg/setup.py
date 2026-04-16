bd09b5f7aad30d86ed184e012b049e77c7b421e2
Maksim Dmitriev <maksim_dmitriev@mymail.sutd.edu.sg>
2026-04-16 09:43:28 +0800

    fix my vision pkg setup

--- src/my_vision_pkg/setup.py
+++ src/my_vision_pkg/setup.py
@@ -1,14 +1,14 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'my_vision_pkg'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
@@ -26,4 +26,4 @@ setup(
             'mission_node = my_vision_pkg.mission_node:main',
         ],
     },
-)s
+)
